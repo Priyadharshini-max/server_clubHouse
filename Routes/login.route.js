a11bd185aa14fc36c62cc49a26b9e071ab5243be
Priya R <priyareddyece97@gmail.com>
2021-09-25 20:35:48 +0530

    first commit

--- Routes/login.route.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const loginService = require("../Services/login.service");
-router.post("/login", loginService.login);
-
-module.exports = router;
--- index.js
+++ index.js
@@ -10,39 +10,11 @@ const io = require('socket.io')(server, {
 })
 
 io.on("connection", (socket) => {
-    console.log("what socket", socket);
-    console.log("Active");
-
     socket.on("chat", (payload) => {
-        console.log("what payload", payload);
         io.emit("chat", payload);
     });
 });
 
-server.listen(5000, () => {
-    console.log("Server  is connected in 5000");
-});
-
-const cors = require("cors");
-
-const mongo = require("./Shared/mongo");
-const loginRoute = require("./Routes/login.route");
-
-app.listen(process.env.PORT, () => {
+server.listen(process.env.PORT, () => {
     console.log(`Connected to ${process.env.PORT} port`);
-})
-
-async function loadApp() {
-    try {
-        await mongo.connect();
-        app.use(cors());
-        app.use(express.json());
-        app.use("/", loginRoute);
-    }
-    catch (err) {
-        console.error(err);
-        process.exit();
-    }
-}
-
-loadApp();
+});
